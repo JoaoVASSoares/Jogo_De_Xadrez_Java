4ec6734be19c3d1d28bb6bb31766c350b8fd1644
João Vitor <joaovitoralmeidas@hotmail.com>
2022-10-24 21:52:07 -0300

    Colocando peças no tabuleiro

---
+++ src/Xadrez/pecas/Rei.java
@@ -0,0 +1,18 @@
+package Xadrez.pecas;
+
+import Xadrez.Cor;
+import Xadrez.PecaDeXadrez;
+import tabuleiroJogo.Tabuleiro;
+
+public class Rei extends PecaDeXadrez {
+
+	public Rei(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+
+}
