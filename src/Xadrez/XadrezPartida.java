a9fb58032c65a3c192103f307a4b7624adf7ae9f
João Vitor <joaovitoralmeidas@hotmail.com>
2022-11-14 10:46:06 -0300

    Rainha

--- src/Xadrez/XadrezPartida.java
+++ src/Xadrez/XadrezPartida.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import Xadrez.pecas.Bispo;
 import Xadrez.pecas.Cavalo;
 import Xadrez.pecas.Peao;
+import Xadrez.pecas.Rainha;
 import Xadrez.pecas.Rei;
 import Xadrez.pecas.Torre;
 import tabuleiroJogo.Peca;
@@ -196,6 +197,7 @@ public class XadrezPartida  {
 		colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
@@ -212,6 +214,7 @@ public class XadrezPartida  {
 		colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
